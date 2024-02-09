2e348b281bc6a930bcf3ce016e7b4208f76ef09c
CaiYiRui <86000592+CaiYiRui@users.noreply.github.com>
2024-02-09 21:02:57 +0800

    Create audio.js

---
+++ js/audio.js
@@ -0,0 +1,9 @@
+
+		window.addEventListener('click',function(){
+			var audio=document.getElementById('au');
+			audio.play();			
+		},false);
+		window.addEventListener('dblclick',function(){
+			var audio=document.getElementById('au');
+			audio.pause();
+		},false);
